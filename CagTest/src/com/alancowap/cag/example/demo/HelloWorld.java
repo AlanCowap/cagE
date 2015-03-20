c0bf8c836f01cd4a68a8c3778f4978943edb3eaa
Alan Cowap <spam@alancowap.com>
2015-03-20 10:40:46 +0000

    Refactor package

---
+++ CagTest/src/com/alancowap/cag/example/demo/HelloWorld.java
@@ -0,0 +1,22 @@
+/** 
+ * Sample code to start off repo
+ * 
+ * 
+ *	 
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies none
+ *  
+ *  
+ */ 
+
+package com.alancowap.cag.example.demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Great Solar eclipse today");
+	}
+
+}
