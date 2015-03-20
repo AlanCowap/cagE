429b836f7863728bb2aa5377f3ed00afa9b10e55
Alan Cowap <spam@alancowap.com>
2015-03-20 11:16:27 +0000

    add runProgram() method

---
+++ CagTest/src/com/alancowap/cag/example/demo/GameApp.java
@@ -0,0 +1,13 @@
+package com.alancowap.cag.example.demo;
+
+public class GameApp {
+
+	public static void main(String[] args) {		
+		new GameApp().runProgram();
+	}
+	
+	private void runProgram(){
+		System.out.println("Hello World");
+	}
+
+}
