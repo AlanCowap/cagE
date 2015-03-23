b41e0ae5ca3daac1e983fa3ec0e8243f338735b2
Alan Cowap <spam@alancowap.com>
2015-03-23 15:39:19 +0000

    comments

--- CagTest/src/com/alancowap/cag/example/demo/GameApp.java
+++ CagTest/src/com/alancowap/cag/example/demo/GameApp.java
@@ -49,6 +49,7 @@ public class GameApp {
 			totalHumanPower += lives[i].getPower();
 			totalRobotPower += lives[++i].getPower();
 		}
+		// output winner of battle
 		if(totalHumanPower > totalRobotPower){
 			System.out.println("Humans win: " + totalHumanPower +" vs " + totalRobotPower);
 		}else if (totalRobotPower > totalHumanPower){
