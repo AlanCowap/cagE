60884bf03070573738b43354aaaf6d4d16c1ee7f
Alan Cowap <AlanCowap@users.noreply.github.com>
2015-03-23 15:44:54 +0000

    Update GameApp.java

--- CagTest/src/com/alancowap/cag/example/demo/GameApp.java
+++ CagTest/src/com/alancowap/cag/example/demo/GameApp.java
@@ -14,6 +14,13 @@ package com.alancowap.cag.example.demo;
 
 import java.util.Random;
 
+/**
+ * This game allows various Lifeforms to do simple battle
+ * Each Lifeform gets a random power, the lifeform with the highest total power wins
+ * 
+ * @author Alan Cowap
+ *
+ */
 public class GameApp {
 	private Lifeform[] lives = new Lifeform[1000];
 			
@@ -29,20 +36,34 @@ public class GameApp {
 			lives[i] = new Human(power, "John Doe"+i);
 			//create robots
 			power = this.getPower(101);
-			lives[++i] = new Robot(power, "TypeA");
-			
+			String robotType ="TypeA";
+			if(power >50 ){
+				robotType = "TypeB";
+			}			
+			lives[++i] = new Robot(power, robotType);			
 		}
-		for(int i=0; i < lives.length; ++i){
-			System.out.println(lives[i]);
+		// Do battle
+		int totalHumanPower=0;
+		int totalRobotPower=0;
+		for(int i=0; i < lives.length-1; ++i){
+			totalHumanPower += lives[i].getPower();
+			totalRobotPower += lives[++i].getPower();
 		}
-		System.out.println("Random: " + this.getPower(101));
+		// Output winner of battle
+		if(totalHumanPower > totalRobotPower){
+			System.out.println("Humans win: " + totalHumanPower +" vs " + totalRobotPower);
+		}else if (totalRobotPower > totalHumanPower){
+			System.out.println("Robots win: " + totalRobotPower +" vs "+ totalHumanPower);
+		}else{
+			System.out.println("It's a draw: " + totalHumanPower +" vs " + totalRobotPower);
+		}
 		
 	}
 	
 	/**
-	 * 
-	 * @param maxPower 
-	 * @return 
+	 * Returns a random int value between 0 (inclusive) and maxPower (exclusive)
+	 * @param maxPower upper limit (exclusive)
+	 * @return random int
 	 */
 	private int getPower(int maxPower){
 		Random r = new Random();
